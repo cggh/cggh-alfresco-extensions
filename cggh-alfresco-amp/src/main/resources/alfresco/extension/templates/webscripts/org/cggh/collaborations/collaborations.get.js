7c80de8a97857bb3aa1853589a76df4a3bc9c277
Ian Wright <ian.wright@well.ox.ac.uk>
2012-12-04 09:19:20 +0000

    Don't error when no permission

--- cggh-alfresco-amp/src/main/resources/alfresco/extension/templates/webscripts/org/cggh/collaborations/collaborations.get.js
+++ cggh-alfresco-amp/src/main/resources/alfresco/extension/templates/webscripts/org/cggh/collaborations/collaborations.get.js
@@ -3,10 +3,12 @@ var folder = null;
 folder = companyhome.childByNamePath('/Sites/sequencing/documentLibrary/Collaborations');
 
 var collabNodes = new Array();
-var allNodesInFolder = folder.children;
-for each (node in allNodesInFolder) {
-    if (node.isSubType("cggh:collaborationFolder")) {
-    	collabNodes.push(node);
+if (folder != null && folder.children != null) {
+    var allNodesInFolder = folder.children;
+    for each (node in allNodesInFolder) {
+        if (node.isSubType("cggh:collaborationFolder")) {
+            collabNodes.push(node);
+        }
     }
 }
 model.collabNodes = collabNodes;
