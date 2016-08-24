f17c049ce5bd150ee81316d78c8da44b2bd17d22
Ian Wright <ian.wright@well.ox.ac.uk>
2016-08-24 09:33:31 +0100

    Change forum editor to allow pasting images - needs testing

---
+++ cggh-share-jar/src/main/resources/alfresco/site-webscripts/org/alfresco/components/discussions/createtopic.get.js
@@ -0,0 +1,23 @@
+function main()
+{
+   // Widget instantiation metadata...
+   var createTopic = {
+      id : "CreateTopic", 
+      name : "Alfresco.CreateTopic",
+      options : {
+         topicId : (page.url.args.topicId != null) ? page.url.args.topicId : "",
+         siteId : (page.url.templateArgs.site != null) ? page.url.templateArgs.site : "",
+         containerId : (page.url.args.containerId != null) ? page.url.args.containerId : "discussions",
+         editMode : (page.url.args.topicId != null),
+         editorConfig : {
+            height: 300,
+            paste_data_images: true,
+            language: this.locale
+         }
+      }
+   };
+   
+   model.widgets = [createTopic];
+}
+
+main();
---
+++ cggh-share-jar/src/main/resources/alfresco/site-webscripts/org/alfresco/components/discussions/replies.get.js
@@ -0,0 +1,22 @@
+function main()
+{
+   // Widget instantiation metadata...
+   var createTopic = {
+      id : "TopicReplies", 
+      name : "Alfresco.TopicReplies",
+      options : {
+         siteId : (page.url.templateArgs.site != null) ? page.url.templateArgs.site : "",
+         containerId : (page.url.args.containerId != null) ? page.url.args.containerId : "discussions",
+         editorConfig : {
+            width: "85%",
+            height: 300,
+            paste_data_images: true,
+            language: this.locale
+         }
+      }
+   };
+   
+   model.widgets = [createTopic];
+}
+
+main();
