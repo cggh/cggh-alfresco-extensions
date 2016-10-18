1a6e0c41ff937ef57d4e8de009eac2497d390820
Ian Wright <ian.wright@well.ox.ac.uk>
2016-10-18 13:24:24 +0100

    Changes to reflect changes in version

--- auth-share-jar/src/main/resources/alfresco/site-webscripts/org/cggh/alfresco/hard-luck/share-header.get.js
+++ auth-share-jar/src/main/resources/alfresco/site-webscripts/org/cggh/alfresco/hard-luck/share-header.get.js
@@ -1,3 +1,10 @@
-model.jsonModel = {
-   widgets: []
-};
+if (!user.isAdmin) {
+	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_PEOPLE");
+	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_MY_FILES");
+	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_SHARED_FILES");
+	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_SITES_MENU");
+	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_TASKS");
+	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_REPOSITORY");
+	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_SEARCH");
+	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_TITLE_BAR");
+}
