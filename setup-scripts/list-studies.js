79a8334089b5e7433dbe3cdd7b57779b80e9d902
Ian Wright <ian.wright@well.ox.ac.uk>
2017-01-25 08:44:35 +0000

    Increase info available when making create studies script

--- setup-scripts/list-studies.js
+++ setup-scripts/list-studies.js
@@ -11,6 +11,17 @@ if (folder != null && folder.children != null) {
           
             logger.log("var collab = folder.createNode(\"" + node.properties["cm:name"] +"\", \"cggh:collaborationFolder\");");
             logger.log("collab.addAspect(\"{http://alfresco.cggh.org/model/custom/1.0}collaborationData\");");
+            logger.log('collab.properties["cggh:samplesExpected"]=' + node.properties["cggh:samplesExpected"] + ";");
+            logger.log('collab.properties["cggh:samplesProcessed"]=' + node.properties["cggh:samplesProcessed"] + ";");
+          if (node.properties["cggh:firstSample"] != null) {
+            logger.log('collab.properties["cggh:firstSample"]="' + utils.toISO8601(node.properties["cggh:firstSample"]) + '";');
+          }
+          if (node.properties["cggh:lastSample"] != null) {
+            logger.log('collab.properties["cggh:lastSample"]="' + utils.toISO8601(node.properties["cggh:lastSample"]) + '";');
+          }
+          if (node.properties["cggh:sampleCountry"] != null) {
+            logger.log('collab.properties["cggh:sampleCountry"]= ["' + node.properties["cggh:sampleCountry"].join('","') + '"];');
+          }
             logger.log("collab.save();");
         }
     }
