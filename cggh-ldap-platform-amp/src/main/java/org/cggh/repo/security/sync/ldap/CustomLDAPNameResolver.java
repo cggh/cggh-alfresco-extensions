b607435222f9bf464ef212c7db11fa64efe58d6e
Ian Wright <ian.wright@well.ox.ac.uk>
2017-02-15 08:48:21 +0000

    Safer processing of alternative login field

--- cggh-ldap-platform-amp/src/main/java/org/cggh/repo/security/sync/ldap/CustomLDAPNameResolver.java
+++ cggh-ldap-platform-amp/src/main/java/org/cggh/repo/security/sync/ldap/CustomLDAPNameResolver.java
@@ -1,8 +1,11 @@
 package org.cggh.repo.security.sync.ldap;
 
+import org.alfresco.repo.security.authentication.AuthenticationDiagnostic;
+import org.alfresco.repo.security.authentication.AuthenticationException;
 import org.alfresco.repo.security.sync.ldap.LDAPNameResolver;
 
 public interface CustomLDAPNameResolver extends LDAPNameResolver {
 
-	public String getAlfUserId();
+	public LDAPResolvedUser getResolvedUser(String userId, AuthenticationDiagnostic diagnostic) throws AuthenticationException;
+
 }
