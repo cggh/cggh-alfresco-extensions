2df387ccc1a4f88d9fe3439a8b2f7fbd3a588fbe
Ian Wright <ian.wright@bdi.ox.ac.uk>
2019-10-08 11:24:59 +0100

    Add -Xverify:all java option

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ then
     echo "Must have /etc/java-cas-client.properties"
     exit 1
 fi
-MAVEN_OPTS="-Xms256m -Xmx2G -noverify -javaagent:${HOME}/.m2/repository/org/springframework/spring-instrument/3.2.14.RELEASE/spring-instrument-3.2.14.RELEASE.jar" mvn clean install -DskipTests=true -Plocal alfresco:run | tee stdout.log
+MAVEN_OPTS="-Xverify:all -Xms256m -Xmx2G -noverify -javaagent:${HOME}/.m2/repository/org/springframework/spring-instrument/3.2.14.RELEASE/spring-instrument-3.2.14.RELEASE.jar" mvn clean install -DskipTests=true -Plocal alfresco:run | tee stdout.log
